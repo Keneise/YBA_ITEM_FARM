d770d599c5bcf67e791074d8f478e5270eb0c92a
Keneise <104054185+Keneise@users.noreply.github.com>
2022-06-09 10:48:15 -0600

    Add files via upload

---
+++ poopoo.lua
@@ -0,0 +1,3 @@
+getgenv().ItemFarm = true  --change the value to false if you want to turn the item farm to stop or change it to true if you want the item farm to run
+getgenv().ServerHop = false -- The serverhop may take atleast 10 - 20 second to run when enabled
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Keneise/YBA_ITEM_FARM/main/dick.lua"))()
